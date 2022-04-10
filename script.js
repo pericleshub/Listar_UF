f0df4af30a28212e46bce4e971d6d7364614a7de
Pericles Souza <periclessouza.pro@gmail.com>
2022-04-10 19:58:43 -0300

    commit

---
+++ script.js
@@ -0,0 +1,69 @@
+// var url = "municipios"
+// fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados/BA/" + url).then(API1 =>{
+//     return API1.json()
+// }).then(API2 =>{
+//     document.getElementById("saida").innerHTML = API2.id
+//     console.log(API2)
+    
+// })
+
+let uf = ""
+let campo = document.querySelector("textarea");
+let but = document.querySelector("button");
+let refresh = 0
+
+but.onclick = function(){
+
+    refresh++;
+    if(refresh > 1){
+        document.location.reload(true);
+    }else
+
+    uf = campo.value
+
+    function getAPI(api){
+        let request = new XMLHttpRequest()
+        request.open("GET", api, false)
+        request.send()
+        return request.responseText
+    }
+
+    function linhaTabela(municipios){
+        console.log(municipios)
+        linha = document.createElement("tr")
+        tdId = document.createElement("td")
+        tdNome = document.createElement("td")
+        tdUF = document.createElement("td")
+        tdRG = document.createElement("td")
+
+        tdId.innerHTML = municipios.id
+        tdNome.innerHTML = municipios.nome
+        tdUF.innerHTML = municipios.microrregiao.mesorregiao.UF.nome
+        tdRG.innerHTML = municipios.microrregiao.mesorregiao.UF.regiao.nome
+
+        linha.appendChild(tdId)
+        linha.appendChild(tdNome)
+        linha.appendChild(tdUF)
+        linha.appendChild(tdRG)
+
+        return linha
+    }
+    
+    function main(){
+        getJSON = getAPI("https://servicodados.ibge.gov.br/api/v1/localidades/estados/" + uf +"/municipios")
+        let municipios = JSON.parse(getJSON)
+        let tabela = document.getElementById("tabela")
+
+        municipios.forEach(element => {
+            let linha = linhaTabela(element)
+            
+            tabela.append(linha)
+            
+        });
+
+    }
+
+    //document.querySelector("button").value = "Atualize a Página";
+    main()
+    
+}
